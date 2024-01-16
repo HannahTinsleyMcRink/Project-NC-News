8183425f6c502d1330105104fde5b737ed198928
Hannah TM <hannahtm1@hotmail.com>
2024-01-16 08:35:08 +0000

    task 3 core GET/api

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+const {getTopics, getEndpoints} = require("./controllers/topics.controllers")
 const express = require("express")
 const app = express()
 app.use(express.json())
 
-const {getTopics} = require("./controllers/topics.controllers")
+
 
 app.get('/api/topics', getTopics)
 
+app.get('/api', getEndpoints)
+
 module.exports = app
+
+// const port = 8080;
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
--- models/topics.models.js
+++ models/topics.models.js
@@ -1,9 +1,23 @@
 const db = require('../db/connection')
+const fs = require('fs/promises')
 
-module.exports.fetchTopics = () => {
+exports.fetchTopics = () => {
     return db
     .query("SELECT * FROM topics;")
     .then(({rows}) => {
         return rows
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+exports.fetchEndpoints = () => {
+    return fs.readFile('./endpoints.json', 'utf8')
+    .then((result) => {
+        return JSON.parse(result)
+    })
+    .catch((err) => {
+        console.log(err)
     })
 }
